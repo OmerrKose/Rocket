073aab0c8c6b32d33953a966d0130526f0532f3b
OmerrKose <omerr.kose99@gmail.com>
2021-03-19 15:53:05 +0300

    failed to implement recyclerView method to the MainActivity

--- app/src/main/java/com/example/rockets/MainActivity.java
+++ app/src/main/java/com/example/rockets/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.rockets;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import kotlin.jvm.internal.markers.KMutableList;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     RestInterface restInterface;
+    private Object Rocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +46,15 @@ public class MainActivity extends AppCompatActivity {
                 //no-op
             }
         });
+
+        public rv; //TODO how to create variables?
+        rv = findViewById(R.id.recyclerView);
+        Adapter rocketAdapter;
+        rocketAdapter = Adapter(KMutableList(Rocket));
+        Context context = this;
+        rv.apply(); //TODO .apply method
+            layoutManager = new LinearLayoutManager(context, RecyclerView.VERTICAL, false);
+            Adapter rocketAdapter1 = rocketAdapter;
+        }
     }
 }
